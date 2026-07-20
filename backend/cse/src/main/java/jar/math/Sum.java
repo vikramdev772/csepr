a1b33816eab60010967955f16fa6cea9b215b1b3
karnakantimayuri <karnakantimayuri@gmail.com>
2026-07-20 14:45:47 +0530

    feat : sum endpoint

---
+++ backend/cse/src/main/java/jar/math/Sum.java
@@ -0,0 +1,12 @@
+package jar.math;
+
+public class Sum{
+
+ public int add(int n){
+            int a=0;
+            for(int i=0;i<=n;i++){
+                a=a+i;
+            }
+            return a;
+    }
+}
